ebe54de8455077d1868316597082fc5f4596a565
drsam94 <drsam94@gmail.com>
2015-11-20 19:41:18 -0500

    trimming by being less efficient, especially on 5

--- golf/pygolf/e4.py
+++ golf/pygolf/e4.py
@@ -1,2 +1,2 @@
-r=range(999,1,-1)
+r=range(999)
 print(max(i*j for i in r for j in r if int(str(i*j)[::-1])==i*j))
--- golf/pygolf/e5.py
+++ golf/pygolf/e5.py
@@ -1,3 +1,3 @@
-p,g=1,lambda a,b:g(b,a%b)if b else a
-for i in range(2,21):p=(p*i)//g(p,i)
-print(p)
+y=8
+while~-all(y%i<1 for i in range(1,20)):y+=8
+print(y)
