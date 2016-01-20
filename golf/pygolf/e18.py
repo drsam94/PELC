dd8d01ba85db626bbded56660299f8b933c9cac2
drsam94 <drsam94@gmail.com>
2016-01-20 11:30:21 -0500

    some more refinement

--- golf/pygolf/e18.py
+++ golf/pygolf/e18.py
@@ -1,4 +1,4 @@
-p=[list(map(int,s[:-1].split(' ')))for s in open('T').readlines()]
+p=[list(map(int,s[:-1].split(' ')))for s in open('T')]
 while p:
  k=p.pop()
  for i in range(len(p)):p[-1][i]+=max(k[i:i+2])
