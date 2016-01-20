97cbf71c2e0683835b4c498c36e23db3f2a769ca
drsam94 <drsam94@gmail.com>
2016-01-19 21:08:34 -0500

    further optimizing, especially e11

--- golf/pygolf/e27.py
+++ golf/pygolf/e27.py
@@ -3,7 +3,7 @@ R=range(-1000,1000)
 for a in R:
  for b in R:
   n=0;p=lambda:n*n+a*n+b
-  while p()>1 and all(p()%i for i in range(2,1+int(p()**.5))):n+=1
+  while p()>1and all(p()%i for i in range(2,int(p()**.5))):n+=1
   if n>M:A=a*b;M=n
 print(A)
 
