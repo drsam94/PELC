e952a8d11c38e0e335d428fd85ab1b2b3b54499a
drsam94 <drsam94@gmail.com>
2016-01-20 10:21:46 -0500

    a little more shaving

--- golf/pygolf/e26.py
+++ golf/pygolf/e26.py
@@ -1,6 +1,7 @@
 a=b=0
 for d in range(2,1000):
  f=x=1;p={}
- while~-(f in p):p[f]=x;f=f*10%d;x+=1
- if x-p[f]>a:a=x-p[f];b=d
+ while~-(f in p):p[f]=x=x+1;f=f*10%d
+ q=x-p[f]
+ if q>a:a=q;b=d
 print(b)
--- golf/pygolf/e27.py
+++ golf/pygolf/e27.py
@@ -2,8 +2,7 @@ A=M=0
 R=range(-1000,1000)
 for a in R:
  for b in R:
-  n=0;p=lambda:n*n+a*n+b
+  n=0;p=lambda:n*(n+a)+b
   while p()>1and all(p()%i for i in range(2,int(p()**.5))):n+=1
   if n>M:A=a*b;M=n
 print(A)
-
