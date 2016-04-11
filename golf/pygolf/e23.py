9a6078af9dd8fcae14303378553313f127b6fb20
drsam94 <drsam94@gmail.com>
2016-04-11 12:48:10 -0400

    23 shaved

--- golf/pygolf/e23.py
+++ golf/pygolf/e23.py
@@ -1,5 +1,4 @@
 R=range
 C=28123
-d=lambda y:1+sum((i+(y/i)*(i*i<y))*(y%i<1)for i in R(2,int(y**.5)+1))
-a=[x for x in R(1,C)if d(x)>x]
+a=[x for x in R(1,C)if 1+sum((i+(x/i)*(i*i<x))*(x%i<1)for i in R(2,int(x**.5)+1))>x]
 print(sum(set(R(C))-{i+j for i in a for j in a}))
